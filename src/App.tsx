8f70baf5679c2afff88ae91341a6420ccce11ce7
gregeva <evang0911@gmail.com>
2023-04-18 02:25:01 -0600

    hooking it up in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Landing from './landing';
+import MovieList from './movies';
+import APIMovieList from './movie/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <APIMovieList />
     </div>
   );
 }
---
+++ src/movie/MovieList.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Movie } from '../types/movie';
+
+function APIMovieList() {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const rsp = await fetch('https://localhost:4000/movie');
+      const temp = await rsp.json();
+      setMovieData(temp);
+    };
+    fetchMovie();
+  }, []);
+
+  return (
+    <>
+      <div className="row">
+        <h4>Joel's Favorite Movies</h4>
+      </div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Year</th>
+            <th>Director</th>
+            <th>Rating</th>
+            <th>Edited</th>
+            <th>Lent To</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movieData.map((m) => (
+            <tr key={m.movieId}>
+              <td>{m.title}</td>
+              <td>{m.category}</td>
+              <td>{m.year}</td>
+              <td>{m.director}</td>
+              <td>{m.rating}</td>
+              <td>{m.edited}</td>
+              <td>{m.lentTo}</td>
+              <td>{m.notes}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default APIMovieList;
