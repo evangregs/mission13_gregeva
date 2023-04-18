1c6c61548477658f8ee122d423986bf28ccdb661
evangregs <96271051+evangregs@users.noreply.github.com>
2023-04-18 02:42:42 -0600

    Add files via upload
    trying to figure out how to do the backend folder connection... uploading the backend stuff from asp.net here

---
+++ banzaPi/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using banzaPi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace banzaPi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp) {
+            context = temp;
+        }
+        public IEnumerable<JoelHiltonMovieCollection> Get()
+        {
+            var x = context.Movies.ToArray();
+            //return context.Movies.Where(Edited="Yes").ToArray();
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ banzaPi/Data/MovieDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace banzaPi.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options) : base(options) { }
+        public DbSet<JoelHiltonMovieCollection> Movies { get; set; }
+    }
+}
